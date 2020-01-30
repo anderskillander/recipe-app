c97d7db9e5d28a18060d929cc14b7b98c98edbe6
anderskillander <55491817+anderskillander@users.noreply.github.com>
2020-01-30 19:01:40 +0100

    final-gh-pages

---
+++ build/precache-manifest.010ea14434c0b7723f2fe546fd5e8c3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf040dfa99b409671e8d8b8b7162db0",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "4d20d725204fe1bfb17e",
+    "url": "/recipe-app/static/css/main.bd3fccc3.chunk.css"
+  },
+  {
+    "revision": "137720e5425a7cc913d8",
+    "url": "/recipe-app/static/js/2.72223848.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.72223848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d20d725204fe1bfb17e",
+    "url": "/recipe-app/static/js/main.06e1ec8b.chunk.js"
+  },
+  {
+    "revision": "148e400df5fd6abd3a3f",
+    "url": "/recipe-app/static/js/runtime-main.5fe0ecde.js"
+  }
+]);
